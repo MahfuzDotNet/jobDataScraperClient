10cc32e4a853ffcfe769acbd45487f393eef6781
mahfuzdotnet <mahfuzfsl@gmail.com>
2025-10-30 03:03:51 +0600

    Your update message

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,21 @@
+// import axios from 'axios';
+
+// const apiClient = axios.create({
+//   baseURL: '/api',
+//   withCredentials: true,
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
+
+// export default apiClient;
+
 import axios from 'axios';
 
+const API_BASE = import.meta.env.VITE_API_URL;
+
 const apiClient = axios.create({
-  baseURL: '/api',
+  baseURL: API_BASE,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
